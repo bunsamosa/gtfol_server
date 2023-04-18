c148cdf67ba36decd76bbc0ee8b08e3b75414353
madhukar01 <mholla8@gmail.com>
2023-04-18 19:25:26 -0400

    feat(db): setup db and collections

---
+++ dbsetup/attribute_builder.py
@@ -0,0 +1,105 @@
+from typing import Any
+from typing import Dict
+
+from appwrite.services.databases import Databases
+
+
+def create_attribute(
+    attribute: str,
+    attr_type: str,
+    metadata: Dict,
+    db: Databases,
+    context: Dict,
+) -> Any:
+    """
+    Create an attribute for a collection
+    :param attribute: attribute name
+    :param attr_type: attribute type
+    :param metadata: attribute metadata
+    :param db: appwrite database instance
+    :param context: context dictionary
+    """
+    # do not pass default parameter for required attributes
+    if attr_type == "datetime":
+        if metadata["required"] or metadata["array"]:
+            return db.create_datetime_attribute(
+                database_id=context["database_id"],
+                collection_id=context["collection_id"],
+                key=attribute,
+                required=metadata["required"],
+                array=metadata["array"],
+            )
+        else:
+            return db.create_datetime_attribute(
+                database_id=context["database_id"],
+                collection_id=context["collection_id"],
+                key=attribute,
+                required=metadata["required"],
+                default=metadata["default"],
+                array=metadata["array"],
+            )
+
+    elif attr_type == "integer":
+        if metadata["required"] or metadata["array"]:
+            return db.create_integer_attribute(
+                database_id=context["database_id"],
+                collection_id=context["collection_id"],
+                key=attribute,
+                required=metadata["required"],
+                min=metadata["min"],
+                max=metadata["max"],
+                array=metadata["array"],
+            )
+        else:
+            return db.create_integer_attribute(
+                database_id=context["database_id"],
+                collection_id=context["collection_id"],
+                key=attribute,
+                required=metadata["required"],
+                min=metadata["min"],
+                max=metadata["max"],
+                default=metadata["default"],
+                array=metadata["array"],
+            )
+
+    elif attr_type == "boolean":
+        if metadata["required"] or metadata["array"]:
+            return db.create_boolean_attribute(
+                database_id=context["database_id"],
+                collection_id=context["collection_id"],
+                key=attribute,
+                required=metadata["required"],
+                array=metadata["array"],
+            )
+        else:
+            return db.create_boolean_attribute(
+                database_id=context["database_id"],
+                collection_id=context["collection_id"],
+                key=attribute,
+                required=metadata["required"],
+                default=metadata["default"],
+                array=metadata["array"],
+            )
+
+    elif attr_type == "string":
+        if metadata["required"] or metadata["array"]:
+            return db.create_string_attribute(
+                database_id=context["database_id"],
+                collection_id=context["collection_id"],
+                key=attribute,
+                size=metadata["size"],
+                required=metadata["required"],
+                array=metadata["array"],
+            )
+        else:
+            return db.create_string_attribute(
+                database_id=context["database_id"],
+                collection_id=context["collection_id"],
+                key=attribute,
+                size=metadata["size"],
+                required=metadata["required"],
+                default=metadata["default"],
+                array=metadata["array"],
+            )
+    else:
+        raise ValueError(f"Invalid attribute type: {attr_type}")
---
+++ dbsetup/tweets.py
@@ -0,0 +1,149 @@
+from appwrite.services.databases import Databases
+
+from dbsetup.attribute_builder import create_attribute
+
+TWEETS_CID = "tweets"
+TWEETS_ATTRIBUTES = {
+    "created_on": {
+        "type": "datetime",
+        "required": True,
+        "default": None,
+        "array": False,
+    },
+    "text": {
+        "type": "string",
+        "size": 500,
+        "required": True,
+        "default": None,
+        "array": False,
+    },
+    "bookmark_count": {
+        "type": "integer",
+        "required": True,
+        "min": 0,
+        "max": 100000000,
+        "default": 0,
+        "array": False,
+    },
+    "quote_counts": {
+        "type": "integer",
+        "required": True,
+        "min": 0,
+        "max": 100000000,
+        "default": 0,
+        "array": False,
+    },
+    "likes": {
+        "type": "integer",
+        "required": True,
+        "min": 0,
+        "max": 100000000,
+        "default": 0,
+        "array": False,
+    },
+    "reply_counts": {
+        "type": "integer",
+        "required": True,
+        "min": 0,
+        "max": 100000000,
+        "default": 0,
+        "array": False,
+    },
+    "retweet_counts": {
+        "type": "integer",
+        "required": True,
+        "min": 0,
+        "max": 100000000,
+        "default": 0,
+        "array": False,
+    },
+    "language": {
+        "type": "string",
+        "size": 10,
+        "required": True,
+        "default": None,
+        "array": False,
+    },
+    "vibe": {
+        "type": "string",
+        "size": 10,
+        "required": False,
+        "default": None,
+        "array": False,
+    },
+    "place": {
+        "type": "string",
+        "size": 1000,
+        "required": False,
+        "default": None,
+        "array": False,
+    },
+    "media": {
+        "type": "string",
+        "size": 1000,
+        "required": False,
+        "default": None,
+        "array": True,
+    },
+    "hashtags": {
+        "type": "string",
+        "size": 1000,
+        "required": False,
+        "default": None,
+        "array": True,
+    },
+    "symbols": {
+        "type": "string",
+        "size": 1000,
+        "required": False,
+        "default": None,
+        "array": True,
+    },
+}
+
+
+def check_collection(db: Databases, context: dict) -> bool:
+    """
+    Check if tweets collection exists
+    :param db: appwrite database instance
+    :param context: context dictionary
+    """
+    collections = db.list_collections(database_id=context["database_id"])
+
+    if not collections:
+        return False
+
+    return any(
+        collection["name"] == "tweets"
+        for collection in collections["collections"]
+    )
+
+
+def setup_collection(db: Databases, context: dict) -> None:
+    """
+    Setup tweets collection and document attributes
+    :param db: appwrite database instance
+    :param context: context dictionary
+    """
+    # check if collection exists
+    if check_collection(db, context):
+        return
+
+    # create collection
+    db.create_collection(
+        database_id=context["database_id"],
+        collection_id=TWEETS_CID,
+        name=TWEETS_CID,
+        document_security=False,
+    )
+
+    # create attributes
+    context["collection_id"] = TWEETS_CID
+    for attribute, metadata in TWEETS_ATTRIBUTES.items():
+        create_attribute(
+            attribute=attribute,
+            attr_type=metadata["type"],
+            metadata=metadata,
+            db=db,
+            context=context,
+        )
