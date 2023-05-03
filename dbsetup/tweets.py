5dd7d4ea2b422de5a693ea4954b3b3d6b11cd318
madhukar01 <mholla8@gmail.com>
2023-05-03 12:08:36 -0400

    feat(leaderboard): leaderboard collection attributes

--- dbsetup/tweets.py
+++ dbsetup/tweets.py
@@ -1,8 +1,3 @@
-from appwrite.services.databases import Databases
-
-from utils.attribute_builder import create_attribute
-
-TWEETS_CID = "tweets"
 TWEETS_ATTRIBUTES = {
     "created_on": {
         "type": "datetime",
@@ -108,49 +103,3 @@ TWEETS_ATTRIBUTES = {
         "array": False,
     },
 }
-
-
-def check_collection(db: Databases, context: dict) -> bool:
-    """
-    Check if tweets collection exists
-    :param db: appwrite database instance
-    :param context: context dictionary
-    """
-    collections = db.list_collections(database_id=context["database_id"])
-
-    if not collections:
-        return False
-
-    return any(
-        collection["name"] == "tweets"
-        for collection in collections["collections"]
-    )
-
-
-def setup_collection(db: Databases, context: dict) -> None:
-    """
-    Setup tweets collection and document attributes
-    :param db: appwrite database instance
-    :param context: context dictionary
-    """
-    # check if collection exists
-    if check_collection(db, context):
-        return
-
-    # create collection
-    db.create_collection(
-        database_id=context["database_id"],
-        collection_id=context["collection_id"],
-        name=context["collection_name"],
-        document_security=False,
-    )
-
-    # create attributes
-    for attribute, metadata in TWEETS_ATTRIBUTES.items():
-        create_attribute(
-            attribute=attribute,
-            attr_type=metadata["type"],
-            metadata=metadata,
-            db=db,
-            context=context,
-        )
